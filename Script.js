79e5865624e48ca8e4e6476df5a54163a8864960
EduardoAlves0621 <77906596+EduardoAlves0621@users.noreply.github.com>
2024-11-17 19:17:46 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,20 @@
+export const bacteriaData = [
+  {
+    name: "Escherichia coli (E. coli)",
+    description: "Uma bactéria comum encontrada no intestino humano.",
+    antibiotics: ["Amoxicilina", "Ciprofloxacina", "Tetraciclina"],
+    resistantTo: ["Amoxicilina"],
+  },
+  {
+    name: "Staphylococcus aureus",
+    description: "Causa infecções cutâneas e outros problemas graves.",
+    antibiotics: ["Vancomicina", "Eritromicina", "Penicilina"],
+    resistantTo: ["Penicilina"],
+  },
+  {
+    name: "Klebsiella pneumoniae",
+    description: "Uma bactéria que pode causar pneumonia.",
+    antibiotics: ["Ceftriaxona", "Gentamicina", "Colistina"],
+    resistantTo: ["Ceftriaxona", "Gentamicina"],
+  },
+];
